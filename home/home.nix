1716795d0d7735c5e489fa244d9692cb4cef7039
BloodstainedOath <rishabh56847@gmail.com>
2025-06-04 23:24:34 +0530

    Update

--- home/home.nix
+++ home/home.nix
@@ -67,75 +67,73 @@
     fzf
   ];
 
-   programs.zsh = {
-  enable = true;
-
-  oh-my-zsh = {
+  programs.zsh = {
     enable = true;
-    theme = "robbyrussell";
-    plugins = [
-      "git"
-      "sudo"
-      "extract"
-      "colored-man-pages"
-      "aliases"
-    ];
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+        "sudo"
+        "extract"
+        "colored-man-pages"
+        "aliases"
+      ];
+    };
+
+    initContent = ''
+      # Nix store plugins
+      source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+      source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+      # Aliases
+      alias ls='lsd'
+      alias cat='bat'
+      alias fzf='fzf --exact'
+      alias up='cd /home/hunter/.dots && nix flake update'
+      alias update='cd /home/hunter/.dots && sudo nixos-rebuild switch --flake .'
+      alias py='cd /home/hunter/.dots && nix develop .#python --command zsh'
+      alias crust='cd /home/hunter/.dots && nix develop .#cppRust --command zsh'
+    '';
   };
 
-  initContent = ''
-    # Nix store plugins
-    source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-    source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-    # Aliases
-    alias ls='lsd'
-    alias cat='bat'
-    alias fzf='fzf --exact'
-    alias up='cd /home/hunter/.dots && nix flake update'
-    alias update='cd /home/hunter/.dots && sudo nixos-rebuild switch --flake .'
-    alias py='cd /home/hunter/.dots && nix develop .#python --command zsh'
-    alias crust='cd /home/hunter/.dots && nix develop .#cppRust --command zsh'
-  '';
-};
-
-gtk = {
-  enable = true;
-  theme.name = "Sweet-Dark";
-  theme.package = pkgs.sweet;
-
-
-iconTheme = {
-    name = "BeautySolar"; # Must match the folder name in ~/.icons or ~/.local/share/icons
-    # Don't add a `package` if you installed it manually
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Sweet-Dark";
+      package = pkgs.sweet;
+    };
+    iconTheme = {
+      name = "BeautySolar";
+    };
+    cursorTheme = {
+      name = "Bibata-Rainbow-Modern";
+      size = 24;
+    };
   };
- cursorTheme = {
-    name = "Bibata-Rainbow-Modern";
-    size = 24;
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
   };
-};
 
-qt = {
-  enable = true;
-  platformTheme.name = "gtk";
-};
+  programs.spicetify =
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in
+    {
+      enable = true;
+      theme = spicePkgs.themes.defaultDynamic;
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        shuffle
+        beautifulLyrics
+        hidePodcasts
+        betterGenres
+      ];
+    };
 
-programs.spicetify =
-let
-  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-in
-{
-  enable = true;
-  theme = spicePkgs.themes.defaultDynamic;
-  enabledExtensions = with spicePkgs.extensions; [
-    adblock
-    shuffle
-    beautifulLyrics
-    hidePodcasts
-    betterGenres
-  ];
-};
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   home.stateVersion = "24.11";
